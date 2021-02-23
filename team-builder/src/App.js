39fd07bae64910a085f250f1a9d4e1ffdb088623
samplesn08 <samplesn08@gmail.com>
2021-02-23 14:57:06 -0500

    mvp complete!

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,30 +1,14 @@
 import './App.css';
 import Form from './Form';
-import React, { useState } from 'react';
+import React from 'react';
 
-function App(props) {
-  const [memberList, setMemberList] = useState([]);
-  function addMember(member) {
-    setMemberList({
-      ...member,
-      name: member.name,
-      email: member.email,
-      role: member.role
-    })
-  }
-  console.log(memberList)
+function App() {
   return (
-    <div className="App">
-      <h1>Add a New Team Member:</h1>
-      <Form addMember={addMember} name={props.name} email={props.email} role={props.role} />
-      <h2>Current Team:</h2>
-      <div>
-        <p>Name: {memberList.name}</p>
-        <p>E-mail: {memberList.email}</p>
-        <p>Role: {memberList.role}</p>
-      </div>
+    <div className='App'>
+      <h1>Add New Member:</h1>
+      <Form />
     </div>
-  );
+  )
 }
 
 export default App;
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
+    
+const teamList = []
+    
+const initialValues = {
+    name: '',
+    email: '',
+    role: ''
+}
 
-function Form(props) {
-    const [member, setMember] = useState({
-        name: '',
-        email: '',
-        role: ''
-      });
+function Form() {
+    const [team, setTeam] = useState(teamList)
+    const [member, setMember] = useState(initialValues);
+
     const handleInputChange = event => {
     setMember({
         ...member,
@@ -14,29 +20,53 @@ function Form(props) {
 }
     const handleSubmit = event => {
         event.preventDefault();
-        props.addMember(member)
+        const newMember = {
+            name: member.name,
+            email: member.email,
+            role: member.role
+        }
+        setTeam(team.concat(newMember))
+        setMember(initialValues)
     }
     return (
         <div className='Form'>
-            <form onSubmit={event => handleSubmit(event)}>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Name:
-                    <input name='name' type='text' onChange={handleInputChange}/>
+                    <input name='name' type='text' onChange={handleInputChange} value={member.name}/>
                 </label>
                 <br></br>
                 <label>
                     E-mail:
-                    <input name='email' type='text' onChange={handleInputChange}/>
+                    <input name='email' type='text' onChange={handleInputChange} value={member.email}/>
                 </label>
                 <br></br>
                 <label>
                     Role:
-                    <input name='role' type='text' onChange={handleInputChange}/>
+                    <select name='role' onChange={handleInputChange} value={member.role}>
+                        <option value=''>--select role--</option>
+                        <option value='Designer'>Designer</option>
+                        <option value='Back-End Developer'>Back-end Developer</option>
+                        <option value='Front-End Developer'>Front-end Developer</option>
+                        <option value='Team Lead'>Team Lead</option>
+                    </select>
                 </label>
                 <br></br>
                 <button>Add Member</button>
             </form>
+            <div>
+                <h2>Current Team:</h2>
+                {team.map((newPerson, index) => {
+                    return <div key={index}>
+                        Name: {newPerson.name}<br></br>
+                        E-mail: {newPerson.email}<br></br>
+                        Role: {newPerson.role}<br></br>
+                        <br></br>
+                    </div>
+                })}
+            </div>
         </div>
+        
     )
 }
 
